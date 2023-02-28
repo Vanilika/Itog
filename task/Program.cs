563a8ffb32de8b84573bb22336b867a1b7154c87
valya <vanilik@mail.ru>
2023-02-28 23:52:24 +0300

    решила задачу

---
+++ task/Program.cs
@@ -0,0 +1,17 @@
+﻿// 4. Написать программу решающую поставленую задачу:
+// из имеющегося массива строк формирует массив из строк, длина которых меньше или равна 3 символам.
+
+string[] existingarray = new string[] {"один", "два", "три", "four", "five", "6", "seven", "8", "nine"};
+string[] result = new string[existingarray.Length];
+int size = 3;
+int count = 0;
+
+for (int i = 0; i < existingarray.Length; i++)
+{
+    if(existingarray[i].Length <= size)
+    {
+        result[count] = existingarray[i];
+        Console.Write($"{result[count]} ");
+        count++;
+    }
+}
